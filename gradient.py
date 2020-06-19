d6b11987902313957041d493bb338bda7dc101fd
somraj30 <51226880+somraj30@users.noreply.github.com>
2020-06-19 12:11:42 +0530

    Update gradient.py

--- gradient.py
+++ gradient.py
@@ -36,20 +36,21 @@ for e in range(epochs):
         #       the h together with the output
 
         # TODO: Calculate the output
-        output = None
+        h = np.matmul(weights, x)
+        output = sigmoid(h)
 
         # TODO: Calculate the error
-        error = None
+        error = y - output
 
         # TODO: Calculate the error term
-        error_term = None
+        error_term = error*sigmoid(h)*(1-sigmoid(h))
 
         # TODO: Calculate the change in weights for this sample
         #       and add it to the total weight change
-        del_w += 0
+        del_w += learnrate*error_term*x
 
     # TODO: Update weights using the learning rate and the average change in weights
-    weights += 0
+    weights += del_w
 
     # Printing out the mean square error on the training set
     if e % (epochs / 10) == 0:
